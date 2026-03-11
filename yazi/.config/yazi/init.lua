ad70abb153c318501fb2dad11e1d1240e346c9f9
Hovirix <184756415+Hovirix@users.noreply.github.com>
2026-03-11 13:46:39 +0100

    feat(yazi): add auto-layout plugin

--- yazi/.config/yazi/init.lua
+++ yazi/.config/yazi/init.lua
@@ -1,5 +1,3 @@
 require("recycle-bin"):setup()
-require("full-border"):setup {
-	-- Available values: ui.Border.PLAIN, ui.Border.ROUNDED
-	type = ui.Border.PlAIN,
-}
+require("full-border"):setup()
+require("auto-layout"):setup()
